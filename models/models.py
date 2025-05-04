d8d778aca86676963e7c8496588e7058ec94446a
DavidValencia526 <davidvalencia0526@gmail.com>
2025-05-03 21:41:44 -0500

    Refactor Invitations model to update unique constraints and relationships

--- models/models.py
+++ models/models.py
@@ -13,19 +13,16 @@ class InvitationStates(Base):
 
 class Invitations(Base):
     __tablename__ = 'invitations'
-    __table_args__ = (UniqueConstraint('email', 'farm_id'),)
+    __table_args__ = (UniqueConstraint('invited_user_id', 'entity_type', 'entity_id'),)
 
     invitation_id = Column(Integer, primary_key=True)
-    email = Column(String(150), nullable=False)
-    suggested_role_id = Column(Integer, ForeignKey('roles.role_id'), nullable=False)
+    invited_user_id = Column(Integer, nullable=False)
+    suggested_role_id = Column(Integer, nullable=False)
     invitation_state_id = Column(Integer, ForeignKey('invitation_states.invitation_state_id'), nullable=False)
-    farm_id = Column(Integer, ForeignKey('farms.farm_id'), nullable=False)
-    inviter_user_id = Column(Integer, ForeignKey('users.user_id'), nullable=False)
-    invitation_date = Column(DateTime(timezone=True), nullable=False, server_default=func.now())
-
-    # Relaciones
-    farm = relationship("Farms", back_populates="invitations")
+    entity_type = Column(String(50), nullable=False)
+    entity_id = Column(Integer, nullable=False)
+    inviter_user_id = Column(Integer, nullable=False)
+    invitation_date = Column(DateTime(timezone=True), nullable=False)
+    
+    # Relationship
     state = relationship('InvitationStates', back_populates="invitations")
-    inviter = relationship("Users", foreign_keys=[inviter_user_id], back_populates="created_invitations")
-    notifications = relationship("Notifications", back_populates="invitation")
-    suggested_role = relationship('Roles', back_populates="invitations")
